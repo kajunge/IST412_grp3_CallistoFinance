8c42844d277ce2e73eb9440d20b5e8f07b899051
cml5444 <52017499+cmlef5444@users.noreply.github.com>
2020-06-13 18:17:36 -0400

    Cntls and UIs added
    Some have methods included but not all and not finished

---
+++ src/main/java/Register/CustomerData.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Register;
+
+/**
+ *
+ * @author cjani
+ */
+public class CustomerData {
+    
+    private String email;
+    private String password;
+    
+    private CustomerData (String customerEmail, String customerPassword){
+        this.email = customerEmail;
+        this.password = customerPassword;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
