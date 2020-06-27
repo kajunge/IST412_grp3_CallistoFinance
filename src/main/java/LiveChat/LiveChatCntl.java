c3daaf7f05fc442fc194009638acbc32c56a2797
Kristina <kristinamantha@icloud.com>
2020-06-27 19:42:33 -0400

    Updated live chat cntl

--- src/main/java/LiveChat/LiveChatCntl.java
+++ src/main/java/LiveChat/LiveChatCntl.java
@@ -1,13 +1,48 @@
-
 package LiveChat;
 
 /**
- * This is the Live Chat Controller class. This class provides the methods
- * that allow the chat client to send messages to the server.
+ * This is the Live Chat Controller class. This class provides the methods that
+ * allow the chat client to send messages to the server.
+ *
  * @author Chris Lefebvre
  * @author Scott
  * @author kristinamantha
  */
 public class LiveChatCntl {
-    
+
+    public LiveChatCntl() {
+        
+    }
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
+
